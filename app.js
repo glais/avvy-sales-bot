59d72a25400f8ccfe4500e84d1440217d6f4bdbc
glais <16027769+glais@users.noreply.github.com>
2022-10-15 13:13:48 +1100

    Changed interval from 1 minute to 1 hour

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ function formatAndSendTweet(event) {
 
 // Poll OpenSea every 60 seconds & retrieve all sales for a given collection in either the time since the last sale OR in the last minute
 setInterval(() => {
-    const lastSaleTime = cache.get('lastSaleTime', null) || moment().startOf('minute').subtract(59, "seconds").unix();
+    const lastSaleTime = cache.get('lastSaleTime', null) || moment().startOf('hour').subtract(3599, "seconds").unix();
 
     console.log(`Last sale (in seconds since Unix epoch): ${cache.get('lastSaleTime', null)}`);
 
@@ -75,4 +75,4 @@ setInterval(() => {
     }).catch((error) => {
         console.error(error);
     });
-}, 60000);
+}, 3600000);
